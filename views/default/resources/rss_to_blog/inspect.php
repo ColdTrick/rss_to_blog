37bf13c389abd7b942c89aac88ad6bb3b84c6313
Jerome Bakker <jeabakker@coldtrick.com>
2018-07-12 16:35:43 +0200

    chore: improved inspect view

--- views/default/resources/rss_to_blog/inspect.php
+++ views/default/resources/rss_to_blog/inspect.php
@@ -13,21 +13,26 @@ $title = elgg_echo('inspect:object:rss_to_blog', [$entity->getDisplayName()]);
 try {
 	$reader = RSSReader::factory([
 		'feed_url' => $entity->feed_url,
+		'enable_cache' => false,
 	]);
 	
 	$reader->init();
 	
 	$content = $reader->get_raw_data();
 	if ($content === false) {
+		// see if there was an error
 		$content = elgg_view_message('error', $reader->error());
-	} else {
+	} elseif (!empty($content)) {
 		$content = htmlentities($content, ENT_QUOTES & ENT_XML1, 'UTF-8');
 		if (!empty($content)) {
 			$content = elgg_format_element('pre', ['lang' => 'xml'], $content, ['encode_text' => false]);
-		} else {
-			$content  = elgg_view_message('warning', elgg_echo('rss_to_blog:inspect:unable_to_parse'));
 		}
 	}
+	
+	if (empty($content)) {
+		$content  = elgg_view_message('warning', elgg_echo('rss_to_blog:inspect:unable_to_parse'));
+	}
+	
 } catch (InvalidArgumentException $e) {
 	$content = elgg_view_message('error', $e->getMessage());
 }
