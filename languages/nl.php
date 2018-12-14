ba7c3e85889d764a44abd1695a416038aa4016b1
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-12-14 15:52:25 +0100

    chore: updated dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,34 +1,43 @@
 <?php
+/**
+ * This file was created by Translation Editor v7.0-beta
+ * On 2018-12-14 15:51
+ */
 
-return [
-	
-	'admin:configure_utilities:rss_to_blog' => "RSS naar Blog",
-	'add:object:rss_to_blog' => "Nieuwe RSS naar Blog",
-	'edit:object:rss_to_blog' => "Bewerk RSS naar Blog: %s",
-	
-	'rss_to_blog:last_refresh' => "Laatste synchronisatie",
-	
-	// settings
-	'rss_to_blog:settings:proxy:title' => "RSS-feed lezer instellingen",
-	'rss_to_blog:settings:proxy:host' => "Proxy host",
-	'rss_to_blog:settings:proxy:port' => "Proxy poortnummer",
-	'rss_to_blog:settings:proxy:disable_ssl_verify' => "Schakel SSL verificatie uit",
-	'rss_to_blog:settings:proxy:disable_ssl_verify:help' => "Indien ingeschakeld zal er geen SSL verificatie plaatsvinden tijdens het inlezen van de RSS-feed",
-	
-	// edit form
-	'rss_to_blog:edit:feed_url' => "RSS-feed URL",
-	'rss_to_blog:edit:target_owner_guid' => "Blog eigenaar",
-	'rss_to_blog:edit:target_owner_guid:help' => "Zoek naar een gebruiker welke de eigenaar zal worden van de geïmporteerde blogs",
-	'rss_to_blog:edit:target_container_guid' => "Blog container",
-	'rss_to_blog:edit:target_container_guid:help' => "Laat leeg om de eigenaar te gebruiker, of zoek naar een groep en selecteer deze uit de lijst",
-	'rss_to_blog:edit:target_tags' => "Extra tags voor de blog",
-	'rss_to_blog:edit:target_access_id' => "Blog toegangsniveau",
-	'rss_to_blog:edit:target_access_id:group' => "Groepsleden (indien er een groep is geselecteerd)",
-	'rss_to_blog:edit:target_comments_on' => "Reacties op de blog toestaan",
-	'rss_to_blog:edit:refresh_interval' => "Verversingsinterval",
-	'rss_to_blog:edit:refresh_interval:help' => "Hoe vaak moet de RSS-feed worden gecontroleerd op nieuwe content",
-	
-	// actions
-	'rss_to_blog:action:import:error' => "Er is een fout opgetreden tijdens het importeren van %s",
-	'rss_to_blog:action:import:success' => "%s heeft geleid tot %d nieuwe blogs",
-];
+return array (
+  'item:object:rss_to_blog' => 'RSS to Blog configuratie',
+  'collection:object:rss_to_blog' => 'RSS to Blog configuraties',
+  'collection:object:blog:all:internal' => 'Interne blogs',
+  'collection:object:blog:all:external' => 'Externe blogs',
+  'collection:object:blog:group:internal' => 'Interne groepsblogs',
+  'collection:object:blog:group:external' => 'Externe groepsblogs',
+  'inspect:object:rss_to_blog' => 'Inspect RSS to Blog: %s',
+  'rss_to_blog:inspect' => 'Inspecteer RSS-feed',
+  'rss_to_blog:inspect:unable_to_parse' => 'Onmogelijk om de RSS-feed output te tonen',
+  'rss_to_blog:menu:filter:internal' => 'Intern',
+  'rss_to_blog:menu:filter:external' => 'Extern',
+  'rss_to_blog:settings:split_blogs' => 'Voeg tabs toe aan blog lijsten om intern/externe blogs los te tonen',
+  'rss_to_blog:settings:split_blogs:help' => 'Als dit aan staat zullen er 2 tabs worden toegevoegd aan de lijstpagina\'s met aparte lijsten voor interne en externe blogs.',
+  'admin:configure_utilities:rss_to_blog' => 'RSS naar Blog',
+  'add:object:rss_to_blog' => 'Nieuwe RSS naar Blog',
+  'edit:object:rss_to_blog' => 'Bewerk RSS naar Blog: %s',
+  'rss_to_blog:last_refresh' => 'Laatste synchronisatie',
+  'rss_to_blog:settings:proxy:title' => 'RSS-feed lezer instellingen',
+  'rss_to_blog:settings:proxy:host' => 'Proxy host',
+  'rss_to_blog:settings:proxy:port' => 'Proxy poortnummer',
+  'rss_to_blog:settings:proxy:disable_ssl_verify' => 'Schakel SSL verificatie uit',
+  'rss_to_blog:settings:proxy:disable_ssl_verify:help' => 'Indien ingeschakeld zal er geen SSL verificatie plaatsvinden tijdens het inlezen van de RSS-feed',
+  'rss_to_blog:edit:feed_url' => 'RSS-feed URL',
+  'rss_to_blog:edit:target_owner_guid' => 'Blog eigenaar',
+  'rss_to_blog:edit:target_owner_guid:help' => 'Zoek naar een gebruiker welke de eigenaar zal worden van de geïmporteerde blogs',
+  'rss_to_blog:edit:target_container_guid' => 'Blog container',
+  'rss_to_blog:edit:target_container_guid:help' => 'Laat leeg om de eigenaar te gebruiker, of zoek naar een groep en selecteer deze uit de lijst',
+  'rss_to_blog:edit:target_tags' => 'Extra tags voor de blog',
+  'rss_to_blog:edit:target_access_id' => 'Blog toegangsniveau',
+  'rss_to_blog:edit:target_access_id:group' => 'Groepsleden (indien er een groep is geselecteerd)',
+  'rss_to_blog:edit:target_comments_on' => 'Reacties op de blog toestaan',
+  'rss_to_blog:edit:refresh_interval' => 'Verversingsinterval',
+  'rss_to_blog:edit:refresh_interval:help' => 'Hoe vaak moet de RSS-feed worden gecontroleerd op nieuwe content',
+  'rss_to_blog:action:import:error' => 'Er is een fout opgetreden tijdens het importeren van %s',
+  'rss_to_blog:action:import:success' => '%s heeft geleid tot %d nieuwe blogs',
+);
