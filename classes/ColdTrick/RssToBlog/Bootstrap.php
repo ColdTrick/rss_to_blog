18bcb1b2637351f88e1ed0f4bae8188eb34bf04d
Jerome Bakker <jeabakker@coldtrick.com>
2019-03-12 15:47:00 +0100

    chore: improved stability

--- classes/ColdTrick/RssToBlog/Bootstrap.php
+++ classes/ColdTrick/RssToBlog/Bootstrap.php
@@ -3,6 +3,7 @@
 namespace ColdTrick\RssToBlog;
 
 use Elgg\DefaultPluginBootstrap;
+use Symfony\Component\Routing\Route;
 
 class Bootstrap extends DefaultPluginBootstrap {
 	
@@ -39,26 +40,32 @@ class Bootstrap extends DefaultPluginBootstrap {
 		
 		// rewrite all (eg. /blog/all)
 		$all = $route_collection->get('collection:object:blog:all');
-		$all->setDefault('_resource', $internal->getDefault('_resource'));
-		$all->setDefault('filter', $internal->getDefault('filter'));
-		
-		$route_collection->add('collection:object:blog:all', $all);
+		if ($all instanceof Route) {
+			$all->setDefault('_resource', $internal->getDefault('_resource'));
+			$all->setDefault('filter', $internal->getDefault('filter'));
+			
+			$route_collection->add('collection:object:blog:all', $all);
+		}
 		
 		// rewrite default (eg. /blog)
 		$default = $route_collection->get('default:object:blog');
-		$default->setDefault('_resource', $internal->getDefault('_resource'));
-		$default->setDefault('filter', $internal->getDefault('filter'));
-		
-		$route_collection->add('default:object:blog', $default);
+		if ($default instanceof Route) {
+			$default->setDefault('_resource', $internal->getDefault('_resource'));
+			$default->setDefault('filter', $internal->getDefault('filter'));
+			
+			$route_collection->add('default:object:blog', $default);
+		}
 		
 		// rewrite group (eg. /blog/group/{guid}
 		$internal = $route_collection->get('collection:object:blog:group:internal');
 		
 		$group = $route_collection->get('collection:object:blog:group');
-		$group->setDefault('_resource', $internal->getDefault('_resource'));
-		$group->setDefault('filter', $internal->getDefault('filter'));
-		$group->setDefault('subpage', $internal->getDefault('filter'));
-		
-		$route_collection->add('collection:object:blog:group', $group);
+		if ($group instanceof Route) {
+			$group->setDefault('_resource', $internal->getDefault('_resource'));
+			$group->setDefault('filter', $internal->getDefault('filter'));
+			$group->setDefault('subpage', $internal->getDefault('filter'));
+			
+			$route_collection->add('collection:object:blog:group', $group);
+		}
 	}
 }
